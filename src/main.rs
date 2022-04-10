6a3a2ff0161cbbd21ab6ef3e021eae1f3a7fadf1
trendstyle <103385673+trendstyle@users.noreply.github.com>
2022-04-11 04:03:45 +0800

    Add source code from Fiddle

--- src/main.rs
+++ src/main.rs
@@ -1,68 +1,25 @@
-//! Default Compute@Edge template program.
-
-use fastly::http::{header, Method, StatusCode};
-use fastly::{mime, Error, Request, Response};
-
-/// The entry point for your application.
-///
-/// This function is triggered when your service receives a client request. It could be used to
-/// route based on the request properties (such as method or path), send the request to a backend,
-/// make completely new requests, and/or generate synthetic responses.
-///
-/// If `main` returns an error, a 500 error response will be delivered to the client.
+use fastly::error::anyhow;
+use fastly::geo::geo_lookup;
+use fastly::{Error, Request, Response};
+use serde_json::json;
 
 #[fastly::main]
 fn main(req: Request) -> Result<Response, Error> {
-    // Filter request methods...
-    match req.get_method() {
-        // Allow GET and HEAD requests.
-        &Method::GET | &Method::HEAD => (),
+    let client_ip = req
+        .get_client_ip_addr()
+        .ok_or_else(|| anyhow!("could not get client ip"))?;
+    let geo = geo_lookup(client_ip).ok_or_else(|| anyhow!("no geographic data available"))?;
+    let body = json!({
+        "as": {
+            "name": geo.as_name(),
+        },
+        "geo" : {
+            "city": geo.city(),
+            "client_ip": client_ip,
+            "country_name": geo.country_name(),
+            "gmt_offset:": geo.utc_offset().unwrap().to_string(),
+        },
+    });
 
-        // Deny anything else.
-        _ => {
-            return Ok(Response::from_status(StatusCode::METHOD_NOT_ALLOWED)
-                .with_header(header::ALLOW, "GET, HEAD")
-                .with_body_text_plain("This method is not allowed\n"))
-        }
-    };
-
-    // Pattern match on the path...
-    match req.get_path() {
-        // If request is to the `/` path...
-        "/" => {
-            // Below are some common patterns for Compute@Edge services using Rust.
-            // Head to https://developer.fastly.com/learning/compute/rust/ to discover more.
-
-            // Create a new request.
-            // let mut bereq = Request::get("http://httpbin.org/headers")
-            //     .with_header("X-Custom-Header", "Welcome to Compute@Edge!")
-            //     .with_ttl(60);
-
-            // Add request headers.
-            // bereq.set_header(
-            //     "X-Another-Custom-Header",
-            //     "Recommended reading: https://developer.fastly.com/learning/compute",
-            // );
-
-            // Forward the request to a backend.
-            // let mut beresp = bereq.send("backend_name")?;
-
-            // Remove response headers.
-            // beresp.remove_header("X-Another-Custom-Header");
-
-            // Log to a Fastly endpoint.
-            // use std::io::Write;
-            // let mut endpoint = fastly::log::Endpoint::from_name("my_endpoint");
-            // writeln!(endpoint, "Hello from the edge!").unwrap();
-
-            // Send a default synthetic response.
-            Ok(Response::from_status(StatusCode::OK)
-                .with_content_type(mime::TEXT_HTML_UTF_8)
-                .with_body(include_str!("welcome-to-compute@edge.html")))
-        }
-
-        // Catch all other requests and return a 404.
-        _ => Ok(Response::from_status(StatusCode::NOT_FOUND)
-            .with_body_text_plain("The page you requested could not be found\n")),
-    }
+    Ok(Response::new().with_body_json(&body)?)
 }
